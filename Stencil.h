12e428cbfaa0ae61dd357d0e649da34c93af1e92
hjc00 <627586727@qq.com>
2022-08-16 23:40:03 +0800

    Update Stencil.h

--- Stencil.h
+++ Stencil.h
@@ -8,48 +8,48 @@ void RenderStencil(Camera& camera)
 	GLFWwindow* window = Window::InitWindow(1280, 960, camera);
 
 	float vertices[] = {
-		// positions          // normals           // texture coords
-	  -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-	   0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
-	   0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-	   0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-	  -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
-	  -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-
-	  -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
-	   0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
-	   0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
-	   0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
-	  -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
-	  -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
-
-	  -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-	  -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
-	  -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-	  -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-	  -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
-	  -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-
-	   0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-	   0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
-	   0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-	   0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
-	   0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
-	   0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-
-	  -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
-	   0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
-	   0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
-	   0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
-	  -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
-	  -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
-
-	  -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
-	   0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
-	   0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-	   0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
-	  -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
-	  -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+		// Back face
+		   -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+			0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right         
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+		   -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom-left
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+		   // Front face
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+		   -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+		   // Left face
+		   -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+		   -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-left
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+		   -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+		   // Right face
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right         
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+			0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left     
+		   // Bottom face
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+			0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // top-left
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+		   -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+		   -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+		   // Top face
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right     
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+		   -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+		   -0.5f,  0.5f,  0.5f,  0.0f, 0.0f  // bottom-left     
 	};
 
 	glm::vec3 cubePositions[] = {
@@ -98,6 +98,9 @@ void RenderStencil(Camera& camera)
 	objectShader.setInt("mainTex", 0);
 
 	Shader outlineShader("ShaderFile/unlit.vs", "ShaderFile/outline.frag");
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
 
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
